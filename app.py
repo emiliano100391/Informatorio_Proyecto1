d66c20596bc9f8fa7171f60a82b8d90ba489ec85
emiliano100391 <emiliano100391@gmail.com>
2024-08-11 19:22:01 -0300

    commit2

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from datetime import *
 import MenuDesplegable as MD
-import Persona as Persona
-import Alumnos as Alumnos
-import Docentes as Docentes
+#import Persona as Persona
+#import Alumnos as Alumnos
+#import Docentes as Docentes
 
 app = tk.Tk()
 
 app.configure(width=800, height=500, bg='white')
+app.title('Registro Escolar')
+
 app.mainloop()
